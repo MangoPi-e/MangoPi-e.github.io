dde18bcc9afebce5ca3d7a3e579a11f6ec22633e
Mango <themango.2007@gmail.com>
2026-03-06 07:39:32 +0300

    Meow Meow Meow

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main style={{ padding: 40 }}>
-      <h1>Hello from MangoPi-e 🚀</h1>
-      <p>This is my Next.js + GitHub Pages site.</p>
+      <h1>Neow</h1>
+      <p>Meow Meow</p>
     </main>
   );
 }
